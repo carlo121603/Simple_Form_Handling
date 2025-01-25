f0dadb03a6a2c79ff77ce32cd9d762abbcbcdb0f
Carlo Untalan <152079967+carlo121603@users.noreply.github.com>
2025-01-25 10:12:31 +0800

    Update submit.php

--- submit.php
+++ submit.php
@@ -12,59 +12,10 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
   echo "This is my age: $age";
 
     if(empty($firstName) || empty($secondName) || empty($age)){
-      echo "Please fill in all fields...";
+      echo "<br>Please fill in all fields...Redirecting";
       header("Refresh: 2; URL=http://localhost/wow/index.php");
       exit();
     }
 }else{
   header("Refresh 2; URL=http://localhost/wow/index.php");
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// if($_SERVER["REQUEST_METHOD"] == "POST") {
-//   $firstName = htmlspecialchars($_POST ["first_name"]);
-//   $surName = htmlspecialchars($_POST["last_name"]);
-//   $pet = htmlspecialchars($_POST["favorite_pet"]);
-
-//   echo "This is my first name: $firstName";
-//   echo "<br>";
-//   echo "This is my last name: $surName";
-//   echo "<br>";
-//   echo "This is my favorite pet: $pet";
-
-//   if (empty($firstName)) {
-//     echo "First name is required. Redirecting...";
-//     header("Refresh: 2; URL=http://localhost/wow/");
-//     exit();
-// }
-  
-//   header("Location: http://localhost/wow/");
- 
-// } else {
-//   header("Location: http://localhost/wow/");
-// }
