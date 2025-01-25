272dfee8ef4ca1bc440b8787c4ef5af53bb7fe74
Carlo Untalan <152079967+carlo121603@users.noreply.github.com>
2025-01-25 10:15:20 +0800

    Update index.php

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Simple calculator</title>
+    <title>Simple Form Handling</title>
 </head>
 <body>
     <form action="submit.php" method="POST">
